aa9229768c2d1eb0539d558b0be093790bb3462a
meil <ics.sora.meil@gmail.com>
2020-04-04 20:47:17 +0900

    update sample

--- sample/sample-ui-activity/src/main/kotlin/net/meilcli/sample/activity/MainActivity.kt
+++ sample/sample-ui-activity/src/main/kotlin/net/meilcli/sample/activity/MainActivity.kt
@@ -2,11 +2,17 @@ package net.meilcli.sample.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import net.meilcli.librarian.activities.NoticesActivity
+import net.meilcli.librarian.serializers.NoticesReader
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            startActivity(NoticesActivity.createIntent(this, NoticesReader(), "notices.json"))
+        }
     }
 }
