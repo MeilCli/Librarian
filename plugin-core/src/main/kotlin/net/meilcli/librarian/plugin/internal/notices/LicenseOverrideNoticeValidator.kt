545542f02d88206b358bba9a9abcc347e465bfd4
meil <ics.sora.meil@gmail.com>
2020-05-24 21:38:07 +0900

    fix override by library group

--- plugin-core/src/main/kotlin/net/meilcli/librarian/plugin/internal/notices/LicenseOverrideNoticeValidator.kt
+++ plugin-core/src/main/kotlin/net/meilcli/librarian/plugin/internal/notices/LicenseOverrideNoticeValidator.kt
@@ -7,6 +7,10 @@ import net.meilcli.librarian.plugin.internal.Placeholder
 class LicenseOverrideNoticeValidator : IValidator2<Notice, Notice> {
 
     override fun valid(element1: Notice, element2: Notice): Boolean {
+        if (element2.licenses.isEmpty()) {
+            // no override
+            return true
+        }
         if (element1.licenses.isNotEmpty()) {
             // check same licenses
             for (checkLicense in element1.licenses) {
--- plugin-core/src/main/kotlin/net/meilcli/librarian/plugin/internal/notices/NoticeOverride.kt
+++ plugin-core/src/main/kotlin/net/meilcli/librarian/plugin/internal/notices/NoticeOverride.kt
@@ -11,7 +11,7 @@ class NoticeOverride : IOverride<Notice> {
         val name = if (override.name != Placeholder.name) override.name else source.name
         val author = if (override.author != Placeholder.author) override.author else source.author
         val url = if (override.url != Placeholder.url) override.url else source.url
-        val description = override.description ?: source.description
+        val description = override.description
         val licenses = if (override.licenses.isNotEmpty()) override.licenses else source.licenses
         return Notice(
             artifacts = artifacts,
